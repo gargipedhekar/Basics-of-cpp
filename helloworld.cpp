2b279be0535a1604f92a50d9e28b2ce78079c519
gargipedhekar <gargi27.pedhekar@gmail.com>
2024-07-31 15:12:39 +0530

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,13 @@
+//Name -Gargi Pedhekar
+//PRN - 23070123052
+//Aim - Printing out Hello World
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World";
+    return 0;
+}
+/* Output:
+Hello World
+*/
